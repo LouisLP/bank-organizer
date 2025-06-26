a06906fd0caf5fd8b2b515e5d02dcc2af66dde96
LouisLP <louis.lascelles.palys@gmail.com>
2025-06-26 11:30:22 +0200

    Changing file naming structure to follow "bank organizer"

--- src/main/java/com/bankorganizer/BankOrganizerPlugin.java
+++ src/main/java/com/bankorganizer/BankOrganizerPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.bankorganizer;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -16,13 +16,13 @@ import net.runelite.client.plugins.PluginDescriptor;
 @PluginDescriptor(
 	name = "Example"
 )
-public class ExamplePlugin extends Plugin
+public class BankOrganizerPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private BankOrganizerConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	BankOrganizerConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(BankOrganizerConfig.class);
 	}
 }
